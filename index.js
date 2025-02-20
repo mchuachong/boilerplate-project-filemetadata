7c2f67e27d37b942555a9adea4a398419237108e
Mike <m.chuachong@gmail.com>
2025-02-19 22:50:07 -0500

    commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const parser = require('body-parser')
 var app = express();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
 
 app.use(cors());
+app.use(express.json())
+app.use(parser.urlencoded({extended:true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  console.log(req.file)
+})
 
 
 const port = process.env.PORT || 3000;
